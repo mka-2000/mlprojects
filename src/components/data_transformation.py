a1209346320b95fb35001014484e69ba449126e1
unknown <mkishorearvind2000@gmail.com>
2024-10-30 19:44:08 +0530

    Data Transformation done

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -36,7 +36,7 @@ class DataTransformation:
             num_pipeline = Pipeline(
                 steps=[
                     ("imputer",SimpleImputer(strategy="median")),
-                    ("scaler",StandardScaler())
+                    ("scaler",StandardScaler(with_mean=False))
                 ]
             )
 
@@ -45,7 +45,7 @@ class DataTransformation:
                 steps = [
                     ("imputer",SimpleImputer(strategy="most_frequent")),
                     ("one_hot_encoder",OneHotEncoder()),
-                    ("scaler",StandardScaler())
+                    ("scaler",StandardScaler(with_mean=False))
                 ]
             )
 
