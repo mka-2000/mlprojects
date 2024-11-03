81d31f6c3e8f39b800155d213b36ec7704f4eeb4
unknown <mkishorearvind2000@gmail.com>
2024-11-03 21:07:48 +0530

    pipelines

--- src/utils.py
+++ src/utils.py
@@ -3,7 +3,7 @@ import sys
 
 import numpy as np
 import pandas as pd
-import dill
+import dill 
 from src.exception import CustomException
 from sklearn.metrics import r2_score
 from sklearn.model_selection import train_test_split
@@ -44,4 +44,13 @@ def evaluate_models(x_train,y_train,x_test,y_test,models):
         return report
 
     except Exception as e:
-        raise CustomException(e,sys)    
+        raise CustomException(e,sys)
+
+
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e,sys)        
